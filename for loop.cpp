d58233186b91a2f82f23ec2e0d766fa6da6ce758
anupriya0605 <m.anupriya0605@gmail.com>
2026-04-15 14:39:23 +0530

    Implement number naming and even/odd checking
    This program takes two integers as input and prints the corresponding names for numbers between 1 and 9, and 'even' or 'odd' for numbers greater than 9.

---
+++ for loop.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+
+    
+    string names[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    for (int i = a; i <= b; i++) {
+        if (i >= 1 && i <= 9) {
+            
+            cout << names[i] << endl;
+        } else if (i > 9) {
+            
+            if (i % 2 == 0) {
+                cout << "even" << endl;
+            } else {
+                cout << "odd" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
